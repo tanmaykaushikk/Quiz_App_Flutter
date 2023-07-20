7153602e672697d542fc82a6db4bd1114378808b
Tanmay Kaushik <96286116+tanmaykaushikk@users.noreply.github.com>
2023-07-20 21:58:38 +0530

    Add files via upload

---
+++ quiz.dart
@@ -0,0 +1,69 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/question_screen.dart';
+import 'package:adv_basics/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizClass();
+  }
+}
+
+class _QuizClass extends State<Quiz> {
+  List<String> selectedAnswers = [];
+
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(74, 23, 161, 0.829),
+                  Color.fromRGBO(83, 34, 122, 0.827),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: screenWidget),
+      ),
+    );
+  }
+}
